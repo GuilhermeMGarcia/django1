05dba540ab05badef69d06589f22454f6df18153
G.Garci4 <66149021+GuilhermeMGarcia@users.noreply.github.com>
2022-05-12 23:26:42 -0300

    secretkey
    secretkey

--- Django1/settings.py
+++ Django1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zg)v(na-oi!d#8@4+*jj+9x_#!j)vw&4bj3u9-e6xq)sr2j05k'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
